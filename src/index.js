1ae7033fe47689e908fbb5434fd86022c5aa4ee7
shashank <shashank.srivastava5523@gmail.com>
2022-07-13 23:54:44 +0530

    1st commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import * as ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+
+//const root = ReactDOM.createRoot(
+//  document.getElementById('root')
+//);
+//
+//
+//root.render(element);
 
 
 
