05cdac6548abd581fad4ed800477611f964c6f7a
shashank <shashank.srivastava5523@gmail.com>
2022-07-14 20:43:23 +0530

    4th commikt

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
-import React from 'react'; // get the React object from the react module
+import React,{useEffect,useState} from 'react';
+import '../styles/App.css'
 
 
 const App=()=>{
+//let [count,setCount]=useState(0)
     return (
     <div id="main">
     <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
--- src/index.js
+++ src/index.js
@@ -1,23 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import ReactDOM from "react-dom";
-
-
-
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-//ReactDOM.render(
-//  <App />,
-//  document.getElementById('root')
-
-//const root = ReactDOM.createRoot(
-//  document.getElementById('root')
-//);
-//
-//
-//root.render(element);
 
 
 
